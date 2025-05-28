b944813b9255eae9ca4cc81473c69f7e5b2cb084
avmoonlight <nalivssx@gmail.com>
2025-05-28 08:44:35 -0300

    colocando as cores

--- snapple.py
+++ snapple.py
@@ -4,3 +4,11 @@ import sys
 
 pygame.init()
 pygame.mixer.init()
+
+VERDE = (0, 255, 127)
+VERMELHO = (255, 50, 50)
+AZUL = (30, 144, 255)
+PRETO = (20, 20, 20)
+BRANCO = (255, 255, 255)
+CINZA = (40, 40, 40)
+CINZA_ESCURO = (30, 30, 30)
